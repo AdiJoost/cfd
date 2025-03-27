62df9d5ed8dc4936c2e05ebb9835c079b7e77029
Adrian Joost <adi.joost@gmail.com>
2025-03-27 12:06:38 +0100

    added gStar, ploting

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -1,4 +1,4 @@
-import random
+import matplotlib.pyplot as plt
 import numpy as np
 
 from config.simulationConfig.simulationManager import SimulationManager
@@ -32,14 +32,16 @@ class Simulation():
             print(f"Time: {self.time}")
             deltaTime = self._caluclateDeltaTime()
             self._setBoundaries()
-            self._computeFStar()
-            self._computeGStar()
+            self._computeFStar(deltaTime)
+            self._computeGStar(deltaTime)
             self._updatePressure()
             self._updateU()
             self._updateV()
             self.time += deltaTime
         print(self.uArray)
         print(self.fStar)
+        print(self.gStar)
+        self.plot()
 
     def _caluclateDeltaTime(self) -> float:
         maxFromU = np.max(self.uArray)
@@ -55,14 +57,12 @@ class Simulation():
     def _setBoundaries(self) -> None:
         self.uArray[0, :] = 2 - self.uArray[1, :]
 
-    def _computeFStar(self) -> None:
+    def _computeFStar(self, deltaTime) -> None:
         d2U_dX2 = self._get_d2U_dX2()
         d2U_DY2 = self._get_d2U_dY2()
         dU2_dX = self._get_dU2_dX()
-        self.fStar = (1/ self.reynoldsNumber) *(d2U_dX2 + d2U_DY2) - dU2_dX
-
-    def _computeGStar(self) -> None:
-        pass
+        dUV_dY = self._get_dUV_dy()
+        self.fStar = self.uArray + deltaTime * ((1/ self.reynoldsNumber) *(d2U_dX2 + d2U_DY2) - dU2_dX - dUV_dY)
 
     def _get_d2U_dX2(self):
         result = np.zeros_like(self.uArray)
@@ -83,9 +83,40 @@ class Simulation():
     
     def _get_dUV_dy(self):
         result = np.zeros_like(self.uArray)
-        result[1:-1, :] = (1/self.xLength) * ( ((self.uArray[1:-1,:] + self.uArray[2:,:]) / 2)**2 - ((self.uArray[1:-1,:] + self.uArray[:2,:]) / 2)**2 ) 
+        result[1:-1, :] = (1/self.xLength) * ( ((self.uArray[1:-1,:] + self.uArray[2:,:]) / 2)**2 - ((self.uArray[1:-1,:] + self.uArray[:-2,:]) / 2)**2 ) 
         alphaParam = np.zeros_like(self.uArray)
         alphaParam[1:-1, :] = (self.alpha / self.xLength) * ((np.abs(self.uArray[1:-1,:] + self.uArray[2:,:]) * (self.uArray[1:-1,:] - self.uArray[2:,:]) / 4 ) - (np.abs(self.uArray[1:-1,:] + self.uArray[:-2,:]) * (self.uArray[:-2,:] - self.uArray[1:-1,:]) / 4 ))
+        return result + alphaParam
+
+    def _computeGStar(self, deltaTime) -> None:
+        d2V_dY2 = self._get_d2V_dY2()
+        d2V_dX2 = self._get_d2V_dX2()
+        dV2_dY = self._get_dV2_dY()
+        dUV_dX = self._get_dUV_dX()
+        self.gStar = self.vArray + deltaTime * ((1/ self.reynoldsNumber) *(d2V_dX2 + d2V_dY2) - dV2_dY - dUV_dX)
+
+    def _get_d2V_dX2(self):
+        result = np.zeros_like(self.vArray)
+        result[1:-1, :] = (self.vArray[2:,:] - 2*self.vArray[1:-1, :] + self.vArray[:-2, :]) /(self.xLength**2)
+        return result
+
+    def _get_d2V_dY2(self):
+        result = np.zeros_like(self.vArray)
+        result[:, 1:-1] = (self.vArray[:,2:] - 2*self.vArray[:, 1:-1] + self.vArray[:, :-2]) /(self.yLength**2)
+        return result
+    
+    def _get_dV2_dY(self):
+        result = np.zeros_like(self.vArray)
+        result[:, 1:-1] = (1/self.yLength) * ( ((self.vArray[:,1:-1] + self.vArray[:,2:]) / 2)**2 - ((self.vArray[:,1:-1] + self.vArray[:,:-2]) / 2)**2 ) 
+        alphaParam = np.zeros_like(self.vArray)
+        alphaParam[:, 1:-1] = (self.alpha / self.yLength) * ((np.abs(self.vArray[:,1:-1] + self.vArray[:,2:]) * (self.vArray[:,1:-1] - self.vArray[:,2:]) / 4 ) - (np.abs(self.vArray[:,1:-1] + self.vArray[:,:-2]) * (self.vArray[:,:-2] - self.vArray[:,1:-1]) / 4 ))
+        return result + alphaParam
+    
+    def _get_dUV_dX(self):
+        result = np.zeros_like(self.vArray)
+        result[:, 1:-1] = (1/self.yLength) * ( ((self.vArray[:,1:-1] + self.vArray[:,2:]) / 2)**2 - ((self.vArray[:,1:-1] + self.vArray[:,:-2]) / 2)**2 ) 
+        alphaParam = np.zeros_like(self.vArray)
+        alphaParam[:, 1:-1] = (self.alpha / self.yLength) * ((np.abs(self.vArray[:,1:-1] + self.vArray[:,2:]) * (self.vArray[:,1:-1] - self.vArray[:,2:]) / 4 ) - (np.abs(self.vArray[:,1:-1] + self.vArray[:,:-2]) * (self.vArray[:,:-2] - self.vArray[:,1:-1]) / 4 ))
         return result + alphaParam
 
     def _updatePressure(self) -> None:
@@ -99,3 +130,16 @@ class Simulation():
 
     def _updateV(self) -> None:
         pass
+
+    def plot(self) -> None:
+        fig = plt.figure(figsize=(11, 7), dpi=100)
+        x = np.linspace(0, self.iMax)
+        y = np.linspace(0, self.jMax)
+        X, Y = np.meshgrid(x, y)
+        plt.contourf(X, Y, self.PressureArray, alpha=0.5, cmap="viridis")
+        plt.colorbar()
+        plt.contour(X, Y, self.PressureArray, cmap="viridis")
+        plt.quiver(X[::2, ::2], Y[::2, ::2], self.uArray[::2, ::2], self.vArray[::2, ::2])
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
