8a50f581191e31add1ab542fdb7c20a5bf73471d
Adrian Joost <adi.joost@gmail.com>
2025-04-20 13:38:42 +0200

    stuff

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from config.simulationConfig.simulationManager import SimulationManager
-from src.solvers.idkTheName import step
+from src.solvers.idkTheName import step, stepWithIndices
 
 
 class Simulation():
@@ -27,6 +27,13 @@ class Simulation():
         self.fStar = self.simulationManager.getVArray()
         self.gStar = self.simulationManager.getVArray()
 
+        self.j = slice(1, -1)
+        self.i = slice(1, -1)
+        self.jPuls1 = slice(2, self.jMax)
+        self.iPlus1 = slice(2, self.iMax)
+        self.jMinus1 = slice(0, self.jMax - 2)
+        self.iMinus1 = slice(0, self.iMax - 2)
+
 
     def run(self) -> None:
         while (self.time < self.endTime):
@@ -46,21 +53,22 @@ class Simulation():
         maxFromV = np.max(self.vArray)
         maxFromU = np.abs(maxFromU) if maxFromU != 0 else 1
         maxFromV = np.abs(maxFromV) if maxFromV != 0 else 1
-        deltaTX = self.xLength / maxFromU - 0.001
-        deltaTY = self.yLength / maxFromV - 0.001
+        deltaTX = self.xLength / maxFromU
+        deltaTY = self.yLength / maxFromV
 
-        reynoldsStability = (self.reynoldsNumber / 2) * 1/(1/(self.xLength ** 2) + 1/(self.yLength ** 2))
-        return self.tau * min (reynoldsStability, deltaTX, deltaTY)
+        reynoldsStability = (self.reynoldsNumber / 2) * (( (1/ (self.xLength**2)) + (1 / (self.yLength**2)) ) **-1)
+        return self.tau * min (reynoldsStability, deltaTX, deltaTY, 0.001)
 
     def _setBoundaries(self) -> None:
-        self.uArray[:, 0] = 2 - self.uArray[:, 1]
-        self.uArray[:, self.jMax - 1] = 1 - self.uArray[:, self.jMax - 2]
-        self.uArray[0,:] = 0
-        self.uArray[self.iMax - 1,:] = 0
-        self.vArray[0,:] = 2 - self.vArray[1,:]
-        self.vArray[self.iMax - 1,:] = 2 - self.vArray[self.iMax - 2,:]
-        self.vArray[:, self.jMax - 1] = 0
-        self.vArray[:, 0] = 0
+        self.uArray[:, 0] = 0
+        self.uArray[:, self.iMax - 1] = 0
+        self.uArray[0,:] = - self.uArray[1,:]
+        self.uArray[self.jMax - 1, :] = 2 - self.uArray[self.jMax -2,:]
+
+        self.vArray[0,:] = 0
+        self.vArray[self.jMax - 1,:] = 0
+        self.vArray[:, self.iMax - 1] = - self.vArray[:, self.iMax - 2]
+        self.vArray[:, 0] = - self.vArray[:, 1]
 
     def _computeFStar(self, deltaTime) -> None:
         d2U_dX2 = self._get_d2U_dX2()
@@ -71,26 +79,28 @@ class Simulation():
 
     def _get_d2U_dX2(self):
         result = np.zeros_like(self.uArray)
-        result[1:-1, :] = (self.uArray[2:,:] - 2*self.uArray[1:-1, :] + self.uArray[:-2, :]) /(self.xLength**2)
+        result[:, self.i] = (self.uArray[:,self.iMinus1] - 2*self.uArray[:, self.i] + self.uArray[:, self.iPlus1]) /(self.xLength**2)
         return result
     
     def _get_d2U_dY2(self):
         result = np.zeros_like(self.uArray)
-        result[:, 1:-1] = (self.uArray[:,2:] - 2*self.uArray[:, 1:-1] + self.uArray[:, :-2]) /(self.yLength**2)
+        result[self.j, :] = (self.uArray[self.jPuls1,:] - 2*self.uArray[self.j,:] + self.uArray[self.jMinus1, :]) /(self.yLength**2)
         return result
     
     def _get_dU2_dX(self):
         result = np.zeros_like(self.uArray)
-        result[1:-1, :] = (1/self.xLength) * ( ((self.uArray[1:-1,:] + self.uArray[2:,:]) / 2)**2 - ((self.uArray[1:-1,:] + self.uArray[:-2,:]) / 2)**2 ) 
+        result[:, self.i] = (1/self.xLength) * ( ((self.uArray[:,self.i] + self.uArray[:,self.iPlus1]) / 2)**2 - ((self.uArray[:,self.iMinus1] + self.uArray[:,self.i]) / 2)**2 ) 
         alphaParam = np.zeros_like(self.uArray)
-        alphaParam[1:-1, :] = (self.alpha / self.xLength) * ((np.abs(self.uArray[1:-1,:] + self.uArray[2:,:]) * (self.uArray[1:-1,:] - self.uArray[2:,:]) / 4 ) - (np.abs(self.uArray[1:-1,:] + self.uArray[:-2,:]) * (self.uArray[:-2,:] - self.uArray[1:-1,:]) / 4 ))
+        alphaParam[:, self.i] = (self.alpha / self.xLength) * ((np.abs(self.uArray[:,self.i] + self.uArray[:,self.iPlus1]) / 2 * (self.uArray[:,self.i] - self.uArray[:,self.iPlus1]) / 2 ) - (np.abs(self.uArray[:,self.iMinus1] + self.uArray[:,self.i]) / 2 * (self.uArray[:,self.iMinus1] - self.uArray[:,self.i]) / 2 ))
         return result + alphaParam
     
     def _get_dUV_dy(self):
         result = np.zeros_like(self.uArray)
-        result[1:-1, :] = (1/self.xLength) * ( ((self.uArray[1:-1,:] + self.uArray[2:,:]) / 2)**2 - ((self.uArray[1:-1,:] + self.uArray[:-2,:]) / 2)**2 ) 
+        result[self.j,self.i] = (1/self.yLength) * ((self.vArray[self.j, self.i] + self.vArray[self.j, self.iPlus1]) * (self.uArray[self.j, self.i] + self.uArray[self.jPuls1, self.i]) / 4)
+        result[self.j, self.i] -= (1/self.yLength) * (self.vArray[self.jMinus1, self.i] + self.vArray[self.jMinus1, self.iPlus1]) *(self.uArray[self.jMinus1, self.i] + self.uArray[self.j, self.i]) / 4 
         alphaParam = np.zeros_like(self.uArray)
-        alphaParam[1:-1, :] = (self.alpha / self.xLength) * ((np.abs(self.uArray[1:-1,:] + self.uArray[2:,:]) * (self.uArray[1:-1,:] - self.uArray[2:,:]) / 4 ) - (np.abs(self.uArray[1:-1,:] + self.uArray[:-2,:]) * (self.uArray[:-2,:] - self.uArray[1:-1,:]) / 4 ))
+        alphaParam[self.j,self.i] = (self.alpha / self.yLength) * (np.abs(self.vArray[self.j, self.i] + self.vArray[self.j, self.iPlus1])*(self.uArray[self.j,self.i] - self.uArray[self.jPuls1, self.i])) / 4
+        alphaParam[self.j, self.i] -= (self.alpha / self.yLength) * (np.abs(self.vArray[self.jMinus1, self.i] + self.vArray[self.jMinus1, self.iPlus1])*(self.uArray[self.jMinus1,self.i] - self.uArray[self.j, self.i])) / 4
         return result + alphaParam
 
     def _computeGStar(self, deltaTime) -> None:
@@ -98,49 +108,50 @@ class Simulation():
         d2V_dX2 = self._get_d2V_dX2()
         dV2_dY = self._get_dV2_dY()
         dUV_dX = self._get_dUV_dX()
-        self.gStar = self.vArray + deltaTime * ((1/ self.reynoldsNumber) *(d2V_dX2 + d2V_dY2) - dV2_dY - dUV_dX)
+        self.gStar = self.vArray + deltaTime * ((1/ self.reynoldsNumber) *(d2V_dX2 + d2V_dY2) - dV2_dY - dUV_dX + 9.81)
 
     def _get_d2V_dX2(self):
         result = np.zeros_like(self.vArray)
-        result[1:-1, :] = (self.vArray[2:,:] - 2*self.vArray[1:-1, :] + self.vArray[:-2, :]) /(self.xLength**2)
+        result[:,self.i] = (self.vArray[:,self.iPlus1] - 2*self.vArray[:,self.i] + self.vArray[:, self.iMinus1]) /(self.xLength**2)
         return result
 
     def _get_d2V_dY2(self):
         result = np.zeros_like(self.vArray)
-        result[:, 1:-1] = (self.vArray[:,2:] - 2*self.vArray[:, 1:-1] + self.vArray[:, :-2]) /(self.yLength**2)
+        result[self.j,:] = (self.vArray[self.jPuls1,:] - 2*self.vArray[self.j, :] + self.vArray[self.jMinus1, :]) /(self.yLength**2)
         return result
     
     def _get_dV2_dY(self):
         result = np.zeros_like(self.vArray)
-        result[:, 1:-1] = (1/self.yLength) * ( ((self.vArray[:,1:-1] + self.vArray[:,2:]) / 2)**2 - ((self.vArray[:,1:-1] + self.vArray[:,:-2]) / 2)**2 ) 
+        result[self.j, :] = (1/self.yLength) * ( ((self.vArray[self.j, :] + self.vArray[self.jPuls1,:]) / 2)**2 - (((self.vArray[self.jMinus1,:] + self.vArray[self.j,:]) / 2)**2) ) 
         alphaParam = np.zeros_like(self.vArray)
-        alphaParam[:, 1:-1] = (self.alpha / self.yLength) * ((np.abs(self.vArray[:,1:-1] + self.vArray[:,2:]) * (self.vArray[:,1:-1] - self.vArray[:,2:]) / 4 ) - (np.abs(self.vArray[:,1:-1] + self.vArray[:,:-2]) * (self.vArray[:,:-2] - self.vArray[:,1:-1]) / 4 ))
+        alphaParam[self.j,:] = (self.alpha / self.yLength) * ((np.abs(self.vArray[self.j,:] + self.vArray[self.jPuls1,:]) * (self.vArray[self.j,:] - self.vArray[self.jPuls1,:]) / 4 ) - (np.abs(self.vArray[self.jMinus1,:] + self.vArray[self.j,:]) * (self.vArray[self.jMinus1,:] - self.vArray[self.j,:]) / 4 ))
         return result + alphaParam
     
     def _get_dUV_dX(self):
         result = np.zeros_like(self.vArray)
-        result[:, 1:-1] = (1/self.yLength) * ( ((self.vArray[:,1:-1] + self.vArray[:,2:]) / 2)**2 - ((self.vArray[:,1:-1] + self.vArray[:,:-2]) / 2)**2 ) 
+        result[self.j, self.i] = (1/self.xLength) * ( (self.uArray[self.j, self.i] + self.uArray[self.jPuls1, self.i]) / 2 * (self.vArray[self.j, self.i]+ self.vArray[self.j,self.iPlus1]) / 2)
+        result[self.j, self.i] -= (1/self.xLength) * ( (self.uArray[self.j, self.iMinus1] + self.uArray[self.jPuls1, self.iMinus1]) / 2 * (self.vArray[self.j, self.iMinus1]+ self.vArray[self.j,self.i]) / 2)
         alphaParam = np.zeros_like(self.vArray)
-        alphaParam[:, 1:-1] = (self.alpha / self.yLength) * ((np.abs(self.vArray[:,1:-1] + self.vArray[:,2:]) * (self.vArray[:,1:-1] - self.vArray[:,2:]) / 4 ) - (np.abs(self.vArray[:,1:-1] + self.vArray[:,:-2]) * (self.vArray[:,:-2] - self.vArray[:,1:-1]) / 4 ))
+        alphaParam[self.j, self.i] = (self.alpha / self.xLength) * (np.abs(self.uArray[self.j, self.i] + self.uArray[self.jPuls1,self.i]) / 2) * ((self.vArray[self.j,self.i] - self.vArray[self.j,self.iPlus1]) / 2) - (np.abs(self.uArray[self.j, self.iMinus1] + self.uArray[self.jPuls1,self.iMinus1]) / 2) * ((self.vArray[self.j, self.iMinus1] - self.vArray[self.j,self.i]) / 2)
         return result + alphaParam
 
     def _updatePressure(self, dt) -> None:
         res = float("inf")
         itteration = 0
-        while res > self.epsilon and itteration < 100:
-            step(self.PressureArray, self.fStar, self.gStar, self.xLength, self.yLength, dt)
+        while res > self.epsilon and itteration < 1000:
+            stepWithIndices(self.PressureArray, self.fStar, self.gStar, self.xLength, self.yLength, dt, self.j, self.jMinus1, self.jPuls1, self.i, self.iMinus1, self.iPlus1)
             itteration += 1
         self.PressureArray[0,:] = self.PressureArray[1,:]
-        self.PressureArray[self.iMax - 1, :] = self.PressureArray[self.iMax - 2, :]
+        self.PressureArray[self.jMax - 1, :] = self.PressureArray[self.jMax - 2, :]
         self.PressureArray[:, 0] = self.PressureArray[:, 1]
-        self.PressureArray[:, self.jMax - 1] = self.PressureArray[:, self.jMax - 2]
+        self.PressureArray[:, self.iMax - 1] = self.PressureArray[:, self.iMax - 2]
 
 
     def _updateU(self, dt) -> None:
-        self.uArray[1:,:] = self.fStar[1:,:] - (dt/self.xLength)* (self.PressureArray[1:,:] - self.PressureArray[:-1,:])
+        self.uArray[self.j,self.i] = self.fStar[self.j,self.i] - (dt/self.xLength)* (self.PressureArray[self.j,self.iPlus1] - self.PressureArray[self.j,self.i])
 
     def _updateV(self, dt) -> None:
-        self.vArray[:,1:] = self.gStar[:,1:] - (dt/self.yLength)* (self.PressureArray[:,1:] - self.PressureArray[:,:-1])
+        self.vArray[self.j, self.i] = self.gStar[self.j,self.i] - (dt/self.yLength)* (self.PressureArray[self.jPuls1,self.i] - self.PressureArray[self.j,self.i])
 
     def plot(self) -> None:
         fig = plt.figure(figsize=(11, 7), dpi=100)
--- src/solvers/idkTheName.py
+++ src/solvers/idkTheName.py
@@ -3,9 +3,25 @@ import numpy as np
 def step(pArray, fStar, gStar, dx, dy, dt):
     pd = pArray.copy()
     b = np.zeros_like(gStar)
-    b[1:,:] = dt*(fStar[1:,:] - fStar[:-1,:])
-    b[:,1:] = b[:,1:] + dt*(gStar[:,1:] - gStar[:,:-1]) 
-    pArray[1:-1,1:-1] = (((pd[1:-1, 2:] + pd[1:-1, :-2]) * dy**2 +
-                    (pd[2:, 1:-1] + pd[:-2, 1:-1]) * dx**2 -
+    b[:,1:] = dt*(fStar[:,1:] - fStar[:,:-1])
+    b[1:,:] = b[1:,:] + dt*(gStar[1:,:] - gStar[:-1,:]) 
+    pArray[1:-1,1:-1] = (((pd[2:,1:-1] + pd[:-2,1:-1]) * dy**2 +
+                    (pd[1:-1,2:] + pd[1:-1,:-2]) * dx**2 -
                     b[1:-1, 1:-1] * dx**2 * dy**2) / 
                     (2 * (dx**2 + dy**2)))
+    
+def stepWithIndices(pArray, fStar, gStar, dx, dy, dt, j, jMinus1, jPlus1, i, iMinus1, iPlus1):
+    pArray[j,i] = ((
+                        (1.0/dt
+                            ) * (
+                                ((fStar[j,i] - fStar[j, iMinus1]) * (1.0/dx)
+                                 ) + (
+                                 (gStar[j,i] - gStar[jMinus1, i]) * (1.0/dy)        
+                                 )     
+                            )
+                        ) - (
+                        dy**2 * (pArray[j, iPlus1] + pArray[j, iMinus1])
+                        ) - (
+                        dx**2 * (pArray[jPlus1, i] + pArray[jMinus1, i]))
+                    ) * (
+                    1.0/-4.0)
